c4e8aaedd9292a4c76cd3ef7db8e940fa9535b6d
Francesco Lacriola <francescolacriola@MacBook-Pro-di-Francesco.local>
2020-11-09 00:04:09 +0100

    Facebook Auth  🔑

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -3,11 +3,20 @@ import classes from './Login.module.css';
 import logo from './logo.png';
 import background from './background.png'
 import { Button } from '@material-ui/core';
-import { auth, provider } from '../../firebase';
+import { auth, provider , providerFacebook } from '../../firebase';
 
 function Login() {
-    const signIn = e => {
-        auth.signInWithPopup(provider).catch(err => alert(err.message))
+    const signIn = providerName => e => {
+        let currProvider;
+        if(providerName === 'google'){
+            console.log("google");
+            currProvider = provider;
+        } else {
+            console.log("facebook")
+            currProvider = providerFacebook;
+            currProvider.addScope('public_profile');
+        }
+        auth.signInWithPopup(currProvider).catch(err => alert(err.message))
     }
     return (
         <div className={classes.login} style={{ backgroundImage: `url(${background})` }}>
@@ -15,7 +24,9 @@ function Login() {
                 <div className={classes.login__logo}>
                     <img src={logo} alt="Discord Logo" />
                 </div>
-                <Button onClick={signIn}>Sign In</Button>
+                <Button onClick={signIn('google')}>Sign In With Google</Button>
+                <Button onClick={signIn('facebook')}>Sign In With Facebook</Button>
+
             </div>
         </div>
     )
--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,9 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
+const providerFacebook = new firebase.auth.FacebookAuthProvider();
+//providerFacebook.addScope('public_profile, email, gender, user_birthday');
 
-export { auth, provider };
+export { auth, provider, providerFacebook };
 export default db;
+
